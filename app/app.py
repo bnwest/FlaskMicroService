d9e7c818a1e36f201ebf52528dfa79a213a08430
Brian West <bwest@xometry.com>
2019-07-12 13:08:19 -0400

    add /get_versions

--- app/app.py
+++ app/app.py
@@ -2,12 +2,31 @@
 A Flask Micro Service app.
 """
 
-from flask import Flask
+import sys
+
+from flask import Flask, jsonify
+from flask import __version__ as flask_version
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
-    return 'Flask Dockerized'
+    return 'Flask Dockerized: Hello World.'
+
+
+@app.route('/get_versions')
+def get_versions():
+    versions = {
+        'python': sys.version,
+        'python-info': sys.version_info,
+        'flask': flask_version,
+        # numpy
+        # tensorflow
+        # etc.
+    }
+    return jsonify(versions)
+
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
