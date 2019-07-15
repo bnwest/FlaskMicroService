53a816c01ffccbd5182a28f3061c702f88c555a7
Brian West <bwest@xometry.com>
2019-07-15 14:47:40 -0400

    whitespace

--- tests/test_flask_micro_service.py
+++ tests/test_flask_micro_service.py
@@ -1,11 +1,13 @@
 import requests
 
+
 def test_hello_word():
     r = requests.get("http://flask_micro_service:5000/")
     assert r.status_code == 200
     response_str = r.json()
     assert type(response_str) == str
     assert response_str.find('Hello World') >= 0
+
 
 def test_get_versions():
     r = requests.get("http://flask_micro_service:5000/get_versions")
@@ -17,6 +19,7 @@ def test_get_versions():
     assert 'flask_restplus' in response_json
     assert 'marshmallow' in response_json
 
+
 def test_echo():
     r = requests.get("http://flask_micro_service:5000/echo", json={"answer": 42})
     assert r.status_code == 200
@@ -24,6 +27,7 @@ def test_echo():
     assert type(response_json) == dict
     assert 'answer' in response_json
     assert response_json['answer'] == 42
+
 
 def test_echo_plus():
     r = requests.get("http://flask_micro_service:5000/echo_plus", json={"answer": 42})
