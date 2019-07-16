39facba72590d805a3c496976c8df50d990fba34
Brian West <bwest@xometry.com>
2019-07-16 14:51:58 -0400

    working to demonstrate versioning via restplus namespaces and blueprints+namespaces

---
+++ app/app_version.py
@@ -0,0 +1,216 @@
+"""
+A Flask Micro Service app
+with versions in routes,
+via flask and flask_restplus.
+"""
+
+import sys
+
+import flask
+import flask_restplus
+import marshmallow
+
+###############################################################################
+###############################################################################
+# flask
+###############################################################################
+###############################################################################
+
+app = flask.Flask(__name__)
+
+@app.route('/url_map')
+def url_map():
+    routes= {}
+    map = app.url_map
+    for rule in map._rules:
+        routes[rule.endpoint] = rule.rule
+    return flask.jsonify(routes)
+
+###############################################################################
+# route: /get, no version
+# usage: curl -i -X GET http://localhost:5000/get
+###############################################################################
+
+@app.route('/get')
+def get1():
+    payload = flask.request.json
+    versions = {
+        'flask.request.path':      flask.request.path,
+        'flask.request.full_path': flask.request.full_path,
+        'flask.request.url':       flask.request.url,
+        'flask.request.base_url':  flask.request.base_url,
+        'flask.request.url_root':  flask.request.url_root,
+        'python': sys.version,
+        'python-info': sys.version_info,
+        'flask': flask.__version__,
+        'flask_restplus': flask_restplus.__version__,
+        'marshmallow': marshmallow.__version__,
+        # numpy
+        # tensorflow
+        # etc.
+    }
+    return flask.jsonify(versions)
+
+# app.add_url_rule(rule='/get', endpoint='get1', view_func=get1)
+
+###############################################################################
+# route: /v1/get, hardcoded version 1
+# usage: curl -i -X GET http://localhost:5000/v1/get
+###############################################################################
+
+@app.route('/v1/get')
+def get2():
+    payload = flask.request.json
+    versions = {
+        'flask.request.path': flask.request.path,
+        'flask.request.full_path': flask.request.full_path,
+        'flask.request.url': flask.request.url,
+        'flask.request.base_url': flask.request.base_url,
+        'flask.request.url_root': flask.request.url_root,
+        'python': sys.version,
+        'python-info': sys.version_info,
+        'flask': flask.__version__,
+        'flask_restplus': flask_restplus.__version__,
+        'marshmallow': marshmallow.__version__,
+        # numpy
+        # tensorflow
+        # etc.
+    }
+    return flask.jsonify(versions)
+
+###############################################################################
+# route: /vN/get, variable version N where N > 1
+# usage: curl -i -X GET http://localhost:5000/v2/get
+###############################################################################
+
+@app.route('/v<int:version_id>/get')
+def get3(version_id):
+    payload = flask.request.json
+    versions = {
+        'flask.request.path': flask.request.path,
+        'flask.request.full_path': flask.request.full_path,
+        'flask.request.url': flask.request.url,
+        'flask.request.base_url': flask.request.base_url,
+        'flask.request.url_root': flask.request.url_root,
+        'app': version_id,
+        'python': sys.version,
+        'python-info': sys.version_info,
+        'flask': flask.__version__,
+        'flask_restplus': flask_restplus.__version__,
+        'marshmallow': marshmallow.__version__,
+        # numpy
+        # tensorflow
+        # etc.
+    }
+    return flask.jsonify(versions)
+
+###############################################################################
+###############################################################################
+# flask_restplus
+###############################################################################
+###############################################################################
+
+api = flask_restplus.Api(
+    title = 'Subsystem Service',
+    version = '1.0',
+    description = 'A set of service for the Subsytem',
+    endpoint = 'frp', # does not do a damn thing
+)
+
+###############################################################################
+# route: /frp/get, flask_restplus, no version
+# usage: curl -i -X GET http://localhost:5000/frp/get
+###############################################################################
+
+@api.route('/frp/get')
+class Get4(flask_restplus.Resource):
+    """
+    Usage:
+        curl -i --request GET http://localhost:5000/frp/get
+    """
+    def get(self):
+        payload = flask.request.json
+        versions = {
+            'flask.request.path': flask.request.path,
+            'flask.request.full_path': flask.request.full_path,
+            'flask.request.url': flask.request.url,
+            'flask.request.base_url': flask.request.base_url,
+            'flask.request.url_root': flask.request.url_root,
+            'api': api.version,
+            'python': sys.version,
+            'python-info': sys.version_info,
+            'flask': flask.__version__,
+            'flask_restplus': flask_restplus.__version__,
+            'marshmallow': marshmallow.__version__,
+            # numpy
+            # tensorflow
+            # etc.
+        }
+        return flask.jsonify(versions)
+
+###############################################################################
+###############################################################################
+# flask_restplus namespace
+###############################################################################
+###############################################################################
+
+ns2 = flask_restplus.Namespace(
+    name='Subsystem Service v2',
+    description='A set of service for the Subsytem v2.',
+    path='/v2',
+)
+
+###############################################################################
+# route: /v2/frp/get, flask_restplus, no version
+# usage: curl -i -X GET http://localhost:5000/v2/frp/get
+###############################################################################
+
+@ns2.route('/frp/get')
+class Get5(flask_restplus.Resource):
+    def get(self):
+        payload = flask.request.json
+        versions = {
+            'flask.request.path': flask.request.path,
+            'flask.request.full_path': flask.request.full_path,
+            'flask.request.url': flask.request.url,
+            'flask.request.base_url': flask.request.base_url,
+            'flask.request.url_root': flask.request.url_root,
+            'api': api.version,
+            'python': sys.version,
+            'python-info': sys.version_info,
+            'flask': flask.__version__,
+            'flask_restplus': flask_restplus.__version__,
+            'marshmallow': marshmallow.__version__,
+            # numpy
+            # tensorflow
+            # etc.
+        }
+        return flask.jsonify(versions)
+
+###############################################################################
+# main
+###############################################################################
+
+api.add_namespace(ns2)
+
+api.init_app(app)
+
+"""
+$ curl -X GET http://localhost:5000/url_map
+{
+  "Subsystem Service v2_get5": "/v2/frp/get", 
+  "doc": "/", 
+  "get1": "/get", 
+  "get2": "/v1/get", 
+  "get3": "/v<int:version_id>/get", 
+  "get4": "/frp/get", 
+  "restplus_doc.static": "/swaggerui/<path:filename>", 
+  "root": "/", 
+  "specs": "/swagger.json", 
+  "static": "/static/<path:filename>", 
+  "url_map": "/url_map"
+}
+"""
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')
---
+++ app/app_version_blueprint.py
@@ -0,0 +1,139 @@
+"""
+A Flask Micro Service app
+with versions in routes,
+via flask and flask_restplus.
+"""
+
+import sys
+
+import flask
+import flask_restplus
+import marshmallow
+
+###############################################################################
+###############################################################################
+# flask
+###############################################################################
+###############################################################################
+
+app = flask.Flask(__name__)
+
+@app.route('/url_map')
+def url_map():
+    routes= {}
+    map = app.url_map
+    for rule in map._rules:
+        routes[rule.endpoint] = rule.rule
+    return flask.jsonify(routes)
+
+###############################################################################
+###############################################################################
+# flask blueprint
+###############################################################################
+###############################################################################
+
+v1 = flask.Blueprint(
+    name='v1',
+    import_name=__name__,
+    url_prefix='/v1'
+)
+
+###############################################################################
+# route: /get, version in
+# usage: curl -i -X GET http://localhost:5000/v1/get
+###############################################################################
+
+@v1.route('/get')
+def get1():
+    payload = flask.request.json
+    versions = {
+        'flask.request.url': flask.request.url,
+        'python': sys.version,
+        'python-info': sys.version_info,
+        'flask': flask.__version__,
+        'flask_restplus': flask_restplus.__version__,
+        'marshmallow': marshmallow.__version__,
+        # numpy
+        # tensorflow
+        # etc.
+    }
+    return flask.jsonify(versions)
+
+###############################################################################
+###############################################################################
+# flask_restplus
+###############################################################################
+###############################################################################
+
+subsys = flask.Blueprint(
+    name='Subsystem',
+    import_name=__name__,
+    url_prefix='/subsys'
+)
+
+api = flask_restplus.Api(
+    subsys, # flask.Blueprint
+    title = 'Subsystem Service',
+    version = '1.0',
+    description = 'A set of service for the Subsytem',
+    endpoint = 'bp-frp', # does not do a damn thing
+)
+
+ns2 = flask_restplus.Namespace(
+    name='Subsystem Service v2',
+    description='A set of service for the Subsytem v2.',
+    path='/v2',
+)
+
+###############################################################################
+# route: /get, version in
+# usage: curl -i -X GET http://localhost:5000/subsys/v2/get
+###############################################################################
+
+@ns2.route('/get')
+class Get2(flask_restplus.Resource):
+    def get(self):
+        payload = flask.request.json
+        versions = {
+            'flask.request.url': flask.request.url,
+            'api': api.version,
+            'python': sys.version,
+            'python-info': sys.version_info,
+            'flask': flask.__version__,
+            'flask_restplus': flask_restplus.__version__,
+            'marshmallow': marshmallow.__version__,
+            # numpy
+            # tensorflow
+            # etc.
+        }
+        return flask.jsonify(versions)
+
+###############################################################################
+# main
+###############################################################################
+
+api.add_namespace(ns2)
+
+# Calling Api.init_app() is not required here because registering the blueprint
+# with the app takes care of setting up the routing for the application.
+# api.init_app(app)
+
+app.register_blueprint(v1)
+app.register_blueprint(subsys)
+
+"""
+$ curl -X GET http://localhost:5000/url_map
+{
+  "Subsystem.Subsystem Service v2_get2": "/subsys/v2/get", 
+  "Subsystem.doc": "/subsys/", 
+  "Subsystem.root": "/subsys/", 
+  "Subsystem.specs": "/subsys/swagger.json", 
+  "restplus_doc.static": "/swaggerui/<path:filename>", 
+  "static": "/static/<path:filename>", 
+  "url_map": "/url_map", 
+  "v1.get1": "/v1/get"
+}
+"""
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')
